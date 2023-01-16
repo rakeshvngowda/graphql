524494197125d01babb5aa331eb18b31b781d478
rakesh <rnarasimhaiah@livevox.com>
2023-01-16 09:12:44 +0530

    graphHTTP function added in app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+
+const graphqlHTTP = require("express-graphql");
+
 const app = express();
+
+// bind the express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass in a schema property
+  })
+);
 
 app.listen(4000, () => {
   console.log(`listeing to post for new requests ${process.env.PORT}`);
